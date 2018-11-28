55ee71ee92ca0d4e6f3979e19538529b9343cc40
Jason Hu <jasonhu3@illinois.edu>
2018-11-28 07:10:21 -0600

    Added functionality to the first button added a final answer button changed hardcoded strings to string variables tried to get rid of top bar, gave up, think its fine made the app full screen

--- app/src/main/java/com/example/jasonhu/triviaapp/MainMenu.java
+++ app/src/main/java/com/example/jasonhu/triviaapp/MainMenu.java
@@ -1,13 +1,26 @@
 package com.example.jasonhu.triviaapp;
 
+import android.app.ActionBar;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.Button;
 
 public class MainMenu extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main_menu);
+    }
+
+    public void openTrivia(View v){
+        startActivity(new Intent(MainMenu.this, MainActivity.class));
     }
 }
