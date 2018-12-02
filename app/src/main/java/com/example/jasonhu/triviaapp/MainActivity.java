f94e9fd92f2d2943800fa06ed3db69f1d7772a32
anagnos3 <anagnos3@illinois.edu>
2018-12-01 22:41:22 -0600

    Revamping Layout To Auto-Align, use fields

--- app/src/main/java/com/example/jasonhu/triviaapp/MainActivity.java
+++ app/src/main/java/com/example/jasonhu/triviaapp/MainActivity.java
@@ -15,4 +15,19 @@ public class MainActivity extends AppCompatActivity {
                 WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
     }
+    /*
+     * Instance var list of QGroups
+     * instance int pos
+     * Instance int qlistsize
+     * set counters to 0
+     * Function to make api call, parse returned string into array/list (whatever included method
+     *  does) create qgroupobjects then add to instance list
+     * QGroupObject: List of wrongq strings, one rightq string
+     * nextQ: if pos == qlistsize - 1, call api again to add; gen random value between 0 and size of
+     *  wrongqs to set as cpos, set each list element, increment cpos
+     * OnSubmit: if sel is correct, disable list, set correct as green, display correct
+     *  notification, change skip to next; else disable sel item & set red, display incor not.incre.
+     * OnSkip: increment skipped, call nextQ;
+     *
+     */
 }
